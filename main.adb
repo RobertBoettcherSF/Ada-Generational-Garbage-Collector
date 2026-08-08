c19275e52825871498879071e8220b7183818137
Sternenfisch <105643443+RobertBoettcherSF@users.noreply.github.com>
2026-08-08 14:07:53 +0200

    Create main.adb

---
+++ main.adb
@@ -0,0 +1,7 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Main is
+begin
+   Put_Line ("Garbage Collection implementation compiled successfully.");
+   Put_Line ("Please run the test suite using 'make test'.");
+end Main;
